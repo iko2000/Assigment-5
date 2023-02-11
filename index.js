8c97807a09192aae76564bd4f2b393953088d00c
iko2000 <123463655+iko2000@users.noreply.github.com>
2023-02-11 22:48:58 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function entered() {
+    let firstname = document.getElementById("name-input").value 
+    let lastname = document.getElementById("lastname-input").value
+    alert(firstname + " " + lastname + "- You entered this name do not i deserve 100?");
+    console.log(firstname+lastname)
+}
